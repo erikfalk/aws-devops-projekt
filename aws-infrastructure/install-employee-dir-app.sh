dcfe9d3124fac380ca5828db4aca485c44743a69
Erik Falk <erik-falk@web.de>
2023-07-03 18:50:35 +0200

    pipeline test 4

--- aws-infrastructure/install-employee-dir-app.sh
+++ aws-infrastructure/install-employee-dir-app.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -ex
 yum update
 wget https://github.com/erikfalk/aws-devops-projekt/archive/refs/heads/main.zip
-unzip main.zip
-cp -r aws-devops-projekt-main/FlaskApp/ /FlaskApp
-rm -f main.zip
-rm -rf aws-devops-projekt-main/
-cd FlaskApp/
-yum -y install python3-pip
-pip install -r requirements.txt
-yum -y install stress
-cd /
+# unzip main.zip
+# cp -r aws-devops-projekt-main/FlaskApp/ /FlaskApp
+# rm -f main.zip
+# rm -rf aws-devops-projekt-main/
+# cd FlaskApp/
+# yum -y install python3-pip
+# pip install -r requirements.txt
+# yum -y install stress
+# cd /
 yum install ruby
 wget https://aws-codedeploy-eu-central-1.s3.eu-central-1.amazonaws.com/latest/install
 chmod +x ./install
 ./install auto
-cd FlaskApp/
-export PHOTOS_BUCKET=employee-photo-bucket-ef-24241
-export AWS_DEFAULT_REGION=eu-central-1
-export DYNAMO_MODE=on
-FLASK_APP=application.py /usr/local/bin/flask run --host=0.0.0.0 --port=80
+# cd FlaskApp/
+# export PHOTOS_BUCKET=employee-photo-bucket-ef-24241
+# export AWS_DEFAULT_REGION=eu-central-1
+# export DYNAMO_MODE=on
+# FLASK_APP=application.py /usr/local/bin/flask run --host=0.0.0.0 --port=80
