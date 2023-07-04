90a03947048bd053701bf4bf7acc18ae79a1a56e
Erik Falk <erik-falk@web.de>
2023-07-04 23:22:03 +0200

    app runs now as service

--- scripts/stop_app.sh
+++ scripts/stop_app.sh
@@ -1,7 +1,2 @@
 #!/bin/bash -ex
-if pgrep flask >/dev/null; then
-  echo "Process flask exists, killing it..."
-  pkill flask
-else
-  echo "Process flask does not exist."
-  
+systemctl start flask-server.service
